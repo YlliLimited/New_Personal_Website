a3fddb355f07a7601a8f8227c20d386fc9cef4f7
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-02-22 18:08:12 +0100

    modified:   assets/CSS/index.css 	modified:   assets/JS/script.js 	modified:   index.html

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,35 +1,49 @@
 const hero = {
   heroSections: document.querySelector(".heroBackground"),
-  child1: document.querySelector(".child:nth-child(1)"),
-  child2: document.querySelector(".child:nth-child(2)"),
-  child3: document.querySelector(".child:nth-child(3)"),
-  child4: document.querySelector(".child:nth-child(4)"),
-  hovering: false,
+  child1: document.querySelector(".heroBackground .child:nth-child(1)"),
+  child2: document.querySelector(".heroBackground  .child:nth-child(2)"),
+  child3: document.querySelector(".heroBackground  .child:nth-child(3)"),
+  child4: document.querySelector(".heroBackground  .child:nth-child(4)"),
 
-  hoverEffect : function (beingHovered, other1, other2, other3) {
-    other1.style.backgroundColor = window.getComputedStyle(beingHovered).color;
-    other2.style.backgroundColor = window.getComputedStyle(beingHovered).color;
-    other3.style.backgroundColor = window.getComputedStyle(beingHovered).color;
-
-    // this.normalEffect();
+  hoverEffect : function (beingHovered) {
+    if (beingHovered === this.child1) {
+      this.child2.style.width = "20%";
+      this.child3.style.width = "20%";
+      this.child4.style.width = "20%";
+      this.child1.style.width = "40%";
+    } else if (beingHovered === this.child2) {
+      this.child3.style.width = "20%";
+      this.child1.style.width = "20%";
+      this.child4.style.width = "20%";
+      this.child2.style.width = "40%";
+    } else if (beingHovered === this.child3) {
+      this.child4.style.width = "20%";
+      this.child1.style.width = "20%";
+      this.child2.style.width = "20%";
+      this.child3.style.width = "40%";
+    } else {
+      this.child4.style.width = "40%";
+      this.child1.style.width = "20%";
+      this.child2.style.width = "20%";
+      this.child3.style.width = "20%";
+    }
   },
-
-  normalEffect : function () {
-    this.child1.style.width = "25%";
-    this.child1.style.transition = "width 0.5s";
-    this.child2.style.width = "25%";
-    this.child2.style.transition = "width 0.5s";
-    this.child3.style.width = "25%";
-    this.child3.style.transition = "width 0.5s";
-    this.child4.style.width = "25%";
-    this.child4.style.transition = "width 0.5s";
-  }
 }
 
-hero.child1.onmouseover = function() {hero.hoverEffect(hero.child1, hero.child2, hero.child3, hero.child4);};
-hero.child2.onmouseover = function() {hero.hoverEffect(hero.child2, hero.child1, hero.child3, hero.child4);};
-hero.child3.onmouseover = function() {hero.hoverEffect(hero.child3, hero.child2, hero.child1, hero.child4);};
-hero.child4.onmouseover = function() {hero.hoverEffect(hero.child4, hero.child2, hero.child3, hero.child1);};
+window.addEventListener("mousemove", function() {
+  if (hero.heroSections.matches(":hover")){
+    hero.child1.onmouseover = function() {hero.hoverEffect(hero.child1);};
+    hero.child2.onmouseover = function() {hero.hoverEffect(hero.child2);};
+    hero.child3.onmouseover = function() {hero.hoverEffect(hero.child3);};
+    hero.child4.onmouseover = function() {hero.hoverEffect(hero.child4);};
+    document.querySelector(".heroText").style.textShadow = "1px 1px var(--quantertary-color)";
+} else{
+  hero.child1.style.width = "25%";
+  hero.child2.style.width = "25%";
+  hero.child3.style.width = "25%";
+  hero.child4.style.width = "25%";
 
-
-console.log(window.getComputedStyle(hero.child1).color);
+  document.querySelector(".heroText").style.boxShadow = "0px 0px var(--quantertary-color)";
+}
+console.log(hero.heroSections.matches(":hover"));
+})
